080d1030a1547188dcc89018c7adbefe8decde89
wjl <wjl@LAPTOP-PU12EV2U>
2019-07-15 19:31:43 +0800

    test:logger test

--- latest-service/src/test/java/com/github/jartisan/latest/logger/LoggerTest.java
+++ latest-service/src/test/java/com/github/jartisan/latest/logger/LoggerTest.java
@@ -1,6 +1,10 @@
-package com.github.jartisan.latest.string;
+package com.github.jartisan.latest.logger;
+
+import java.lang.reflect.Field;
+import java.util.Objects;
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -20,7 +24,8 @@ public class LoggerTest {
 	private static Logger logger = LoggerFactory.getLogger(LoggerTest.class);
 	
 	/****
-	  *  微信消息封装
+	 *  getClass().getName() + '@' + Integer.toHexString(hashCode())
+	 *  微信消息体封装
 	 *  [
 	 *	  toUserName=gh_3083ef78d39c
 	 *	  fromUserName=o4sdr1Mh_gNNKuSOqwZt1rien_2I
@@ -40,9 +45,37 @@ public class LoggerTest {
 		msg.setMsg("text");
 		msg.setMsgId("22364525629945296");
 		msg.setContent("hello world");
-		logger.info("EventMessage Initialization Ready... [{}]",msg);
+		logger.info("EventMessage Initialization Ready... ");
     }
+
+	@Test
+	public void testToString(){
+		logger.info("EventMessage body is:{}",msg);
+	}
 	
+	
+	@Test
+	public void testReflection() throws IllegalArgumentException, IllegalAccessException{
+		Field[] fields = msg.getClass().getDeclaredFields();
+		for(int i = 0 ; i < fields.length ; i++) {
+		    //设置是否允许访问，不是修改原来的访问权限修饰词。
+			fields[i].setAccessible(true);
+			if(Objects.nonNull(fields[i].get(msg))) {
+				logger.info("EventMessage  field:[{}]_value:[{}]",fields[i].getName(),fields[i].get(msg));	
+			}
+		}
+	}
+	
+	
+	
+	@Test
+	public void testToStringBuilder(){
+		logger.info("EventMessage body is:{}",ToStringBuilder.reflectionToString(msg,ToStringStyle.MULTI_LINE_STYLE));
+	}
+	
+	/***
+	 * 推荐
+	 */
 	@Test
 	public void testMoreObjectsUtil(){
 		logger.info(ReflectionToStringBuilder.toString(msg, ToStringStyle.MULTI_LINE_STYLE, true, true, true, null));
